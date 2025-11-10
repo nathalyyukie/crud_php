66e6d2faf5496adca54f545de570b25a8665f11a
nathalyyukie <hondanathaly@gmail.com>
2025-11-10 10:45:50 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,5 +1,21 @@
 <?php
-
+/**************************
+ Curso: Engenharia de Software
+ Disciplina: Linguagem e Técnicas de Programacão
+ Professor: Flores
+ Turma: ESOFT-2A
+ Componentes: 
+            25169060-2 - Alex Rafael Ferreira
+            25001459-2 - Eduardo Gritten dos Santos Spohr
+            25177941-2 - Juan Pyerre de Sousa Carvalho 
+            25044068-2 - Legiane Cristina de Souza Oliveira Chagas
+            25164713-2 - Luan Enrico Santana Peça
+            25182011-2 - Miguel Felipe Gazola
+            25181985-2 - Nathaly Yukie Otofuji Honda
+            25181898-2 - Pedro Paulo Rodrigues Mardegam
+ Data: 11 de Novembro de 2025
+ Descritivo: Desenvolvimento de um Sistema CRUD em PHP com MySQL
+ ***************************/
 class Database {
     private $host = "localhost";
     private $db_name = "crud_db";
